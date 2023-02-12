b3d3e6219a160fef831c434d3fa586e9a940620e
Cleyan Sampaio <cleyansam@gmail.com>
2023-02-12 19:36:26 -0300

    :sparkles: feat: Adiciona solução da questão 8 da primeira lista

---
+++ listas/estrutura-sequencial/questao-8.py
@@ -0,0 +1,3 @@
+salario_hora = float(input("Digite o quanto você ganha por hora: "))
+horas_trabalhadas = int(input("Digite a quantidade de horas trabalhadas no mês: "))
+print(f"O seu salário do mês será R${salario_hora * horas_trabalhadas: .2f}")
