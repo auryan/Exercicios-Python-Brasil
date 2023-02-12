5565793a471f04366c07adba7064e4a2590bc2a8
Cleyan Sampaio <cleyansam@gmail.com>
2023-02-12 19:56:18 -0300

    :sparkles: feat: Adiciona solução da questão 11 da primeira lista

---
+++ listas/estrutura-sequencial/questao-11.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite um número INTEIRO: "))
+n2 = int(input("Digite outro número INTEIRO: "))
+n3 = float(input("Agora digite um número REAL: "))
+
+print("\nO produto do dobro do primeiro valor com metade do segundo valor:", ((n1 * 2) * (n2 / 2)))
+print("A soma do triplo do primeiro valor com o terceiro valor é:", ((n1 * 3) + n3))
+print("O terceiro valor elevado ao cubo:", n3 ** 3)
